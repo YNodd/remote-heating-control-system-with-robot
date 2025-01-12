b10b9fcaf8f5f7a9686697234f56ab10d0113136
YNodd <ynodd@localhost.localdomain>
2025-01-12 10:08:48 +0100

    corrected comments

--- ownlabel.py
+++ ownlabel.py
@@ -1,5 +1,5 @@
 
-# kivy-label with white (very light grey) background
+# custom kivy labels
 
 from kivy.app import Builder
 from kivy.uix.label import Label
@@ -7,35 +7,27 @@ from kivy.uix.label import Label
 Builder.load_string("""
 <MyWhiteLabel>:
     font_size: 20
-    #background_color: 1, 1, 1, 1
-    #background_normal: ""
     canvas.before:
         Color:
             rgba: 1, 1, 1, 0.7
-        #Line:    # --- adds a border --- #
-        #    width: 2
-        #    rectangle: self.x, self.y, self.width, self.height
         Rectangle: 
             size: self.width, self.height
             pos: self.x, self.y
             
 <MyWarnLabel>:
     font_size: 20
-    #background_color: 1, 1, 1, 1
-    #background_normal: ""
     canvas.before:
         Color:
-            rgba: 0, 0, 0, 0.9  # 1, 1, 1, 0.7
-        #Line:    # --- adds a border --- #
-        #    width: 2
-        #    rectangle: self.x, self.y, self.width, self.height
+            rgba: 0, 0, 0, 0.9
         Rectangle: 
             size: self.width, self.height
             pos: self.x, self.y
             """)
 
 class MyWhiteLabel(Label):
+    # kivy-label with white (very light grey) background
     pass
 
 class MyWarnLabel(Label):
+    # label with black background
     pass
